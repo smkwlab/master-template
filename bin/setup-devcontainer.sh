4c6f8aab683786b38702d4df4e3327816277feda
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-01-04 22:52:21 +0900

    Update setup-devcontainer.sh

--- bin/setup-devcontainer.sh
+++ bin/setup-devcontainer.sh
@@ -3,7 +3,7 @@ git remote add latex https://github.com/smkwlab/latex-environment.git
 git fetch latex
 
 # '.gitignore' should conflict. Ignore this error and use our one.
-git merge --allow-unrelated-histories -m 'merge LaTeX environment' latex/main && true
+git merge --allow-unrelated-histories -m 'merge LaTeX environment' latex/main || true
 git checkout --ours .gitignore
 
 git add .gitignore
